78a96afdd88325b0b580952ca06497824693774c
dwight rau <dwightrau@yahoo.com>
2026-02-20 01:16:05 -0500

    feat: Seed 8 demo patients with full extraction on first run (P6-007)
    Update EF therapist seed to "Dr. Sarah Mitchell" (PhD, LPC). Overhaul
    start-dev.sh: remove dead SQL INSERT, add conditional 8-patient seeding
    with parallel extraction (~5-8 min). Skips if >= 3 patients exist.

---
+++ src/SessionSight.Infrastructure/Migrations/20260220061053_UpdateTherapistSeedData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SessionSight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTherapistSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Therapists",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                columns: new[] { "Credentials", "LicenseNumber", "Name" },
+                values: new object[] { "PhD, LPC", "LPC-2024-0847", "Dr. Sarah Mitchell" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Therapists",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                columns: new[] { "Credentials", "LicenseNumber", "Name" },
+                values: new object[] { null, null, "Default Therapist" });
+        }
+    }
+}
